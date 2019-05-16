1997a1c872de4c174cda34b17ca5eafc345c579a
Anna-Katharina Drewes <annak.drewes@googlemail.com>
2019-05-16 16:59:24 +0200

    Add propTypes

--- src/cards/Tag.js
+++ src/cards/Tag.js
@@ -1,5 +1,10 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 export default function Tag({tags}) {
     return <p><em>{tags.join(', ')}</em></p>
+}
+
+Tag.propTypes = {
+    tags: PropTypes.array
 }
--- src/create/CreatePage.js
+++ src/create/CreatePage.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import Form from './Form'
 
 export default function CreatePage({createCard, ...props}) {
     return (
         <Form onFormSubmit={createCard} {...props} />
     )
+}
+
+CreatePage.propTypes = {
+    createCard: PropTypes.func.isRequired
 }
